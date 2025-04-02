79cfc4b1b65f5ec7e03b72ea65349e5fb6b40b99
MelanieAppAkademie <melanie.geyer@app-akademie.com>
2025-04-02 17:03:40 +0200

    t2

--- lib/features/app.dart
+++ lib/features/app.dart
@@ -5,23 +5,19 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String name = "Melli";
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Row(
-            spacing: 16,
             children: [
               CircleAvatar(child: Icon(Icons.person)),
-              Text("Hello $name"),
+              Text("Hello User"),
             ],
           ),
         ),
         body: Padding(
-          padding: EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
           child: Column(
-            spacing: 16,
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -29,31 +25,59 @@ class App extends StatelessWidget {
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text("Your Balance", style: TextStyle(fontSize: 20)),
-                      Text("\$25,000.20", style: TextStyle(fontSize: 40)),
+                      Text("Your Balance"),
+                      Text(
+                        "\$25,000.20",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        ),
+                      ),
                     ],
                   ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Icon(Icons.add),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black,
-                      foregroundColor: Colors.white,
-                    ),
-                  ),
+                  FilledButton(onPressed: () {}, child: Icon(Icons.add)),
                 ],
               ),
               Container(
+                padding: const EdgeInsets.all(8),
                 height: 200,
+                width: double.infinity,
                 decoration: BoxDecoration(
-                  color: Color(0xFFb18deb),
+                  color: Color(0xFFa489d6),
                   borderRadius: BorderRadius.circular(20),
                 ),
+                child: Column(crossAxisAlignment: CrossAxisAlignment.start),
+              ),
+              Row(
+                children: [
+                  CircularActions(label: "Wallet", icon: Icon(Icons.wallet)),
+                ],
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class CircularActions extends StatelessWidget {
+  final String label;
+  final Icon icon;
+  const CircularActions({super.key, required this.label, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        IconButton.filled(
+          onPressed: () {},
+          icon: icon,
+          iconSize: 40,
+          padding: EdgeInsets.all(20),
+        ),
+        Text(label),
+      ],
     );
   }
 }
